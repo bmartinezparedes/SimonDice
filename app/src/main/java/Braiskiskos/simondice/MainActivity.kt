7636496d751ec6d861ec9635a541256b97240f6f
Braiskiskos <bmartinezparedes@danielcastelao.org>
2021-10-18 11:27:36 +0200

    inicios del codigo

--- app/src/main/java/Braiskiskos/simondice/MainActivity.kt
+++ app/src/main/java/Braiskiskos/simondice/MainActivity.kt
@@ -2,10 +2,36 @@ package Braiskiskos.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var contadorRondas:Int=0
+        var secuencia= arrayOf(2)//esto no esta bien
+        val empezarPartida: Button = findViewById(R.id.bJugar)
+        empezarPartida.setOnClickListener {
+            //Aqui iria lo que ejecutaria darle empezar a partida
+            Log.i("Estado","Boton Jugar pulsado")
+            mostrarRoonda()
+            ejecutarSecuencia()
+        }
+
+
+    }
+    private fun mostrarRoonda(){
+        Log.i("Estado","Mostrar Numero de Rondas")
+    }
+    private fun ejecutarSecuencia(){
+        Log.i("Estado","Se ejecuta el juego")
+    }
+    private fun mensajeUsuario(){
+        Log.i("Estado","Mensaje por toast al usuario")
+    }
+    private fun comprobarSecuencia(){
+        Log.i("Estado","Comprobar que la secuencia del jugador coincide")
     }
 }
